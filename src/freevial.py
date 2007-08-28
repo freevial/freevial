9062db943208062faf2c1f314fabba222e1c9702
Siegfried-Angel Gevatter Pujals (RainCT) <siggi.gevatter@gmail.com>
2007-08-28 19:06:31 +0200

    Don't hide mouse on debug mode.

--- src/freevial.py
+++ src/freevial.py
@@ -43,10 +43,9 @@ class Freevial:
 		self.dades_joc.pantalla = pygame.display.set_mode( ( self.dades_joc.mida_pantalla_x, self.dades_joc.mida_pantalla_y), 0, 32)
 		pygame.display.set_caption('Freevial')
 		pygame.display.set_icon( pygame.image.load(os.path.join(self.dades_joc.folders['images'], 'logo.png')) )
-
-		pygame.mouse.set_visible( False )
 		
 		if not DEBUG_MODE:
+			pygame.mouse.set_visible( False )
 			pygame.display.toggle_fullscreen()
 		
 		# inicialitzem els sistemes de so i text
