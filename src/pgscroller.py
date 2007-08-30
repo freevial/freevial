65422c96524a38942442925a69f69847ba9e2402
Siegfried-Angel Gevatter Pujals (RainCT) <siggi.gevatter@gmail.com>
2007-08-30 22:20:07 +0200

    License under the GNU GPLv3 or later.

--- src/pgscroller.py
+++ src/pgscroller.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
  
-##########################################
 #
 # Freevial - Superseded Component
+# Iterador de fons mentre el concursant pensa la pregunta
 #
-# Iterador de fons mentre el concursant 
-# pensa la pregunta
-# 
-# Cal sobreposar la pregunta i les respostes
+# Copyright (C) 2007 The Freevial Team
 #
-# Carles 22/08/2007
-# RainCT 27/08/2007
+# By Carles Oriol i Margarit <carles@kumbaworld.com>
+# By Siegfried-Angel Gevatter Pujals <siggi.gevatter@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 import sys, random, pygame
