79d2b8ee2589c91dffd2740e3ebf7bdea97c1115
Siegfried-Angel Gevatter Pujals (RainCT) <sgevatter@ubuntu.cat>
2007-11-24 17:57:34 +0100

    create a script to automatically create tarballs (release.sh)

---
+++ release.sh
@@ -0,0 +1,72 @@
+#! /bin/sh
+
+#
+# Freevial
+# Tarball Generator
+#
+# Copyright (C) 2007 The Freevial Team
+#
+# By Siegfried-Angel Gevatter Pujals <siggi.gevatter@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <version>"
+	exit 1
+fi
+
+# Define variables
+TMPDIR=/tmp/freevial-release
+CURDIR=$(pwd)
+VERSION=$1
+
+# Create temporal directory
+echo "Copying files..."
+mkdir -p $TMPDIR/freevial-$VERSION
+
+# Copy files
+cp COPYING README freevial $TMPDIR/freevial-$VERSION
+cp -r data/ src/ $TMPDIR/freevial-$VERSION
+
+# Remove unneeded stuff
+cd $TMPDIR/freevial-$VERSION/src
+rm -f *.pyc ./*/*.pyc *.~1~ ./*/*.~1~ .hidden ./*/.hidden
+
+# Generate tarball
+echo "Generating tarball..."
+cd $TMPDIR
+tar -czf freevial-$VERSION.tar.gz freevial-$VERSION
+
+# Copy generated tarball
+cd $CURDIR
+if [ -e releases ]
+then
+	if [ ! -d ]; then
+		echo "$0: error: can't create directory «releases», a file with the same name already exists."
+		rm -rf $TMPDIR; exit 1
+	fi
+else
+	mkdir releases
+fi
+cp $TMPDIR/freevial-$VERSION.tar.gz releases/
+
+# Print a success message
+echo "File «$CURDIR/freevial-$VERSION.tar.gz» successfully generated."
+
+# Clean up
+echo "Cleaning up..."
+cd $CURDIR
+rm -rf $TMPDIR
